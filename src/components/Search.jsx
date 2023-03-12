4e1ae5408a074e7fd61ac2995f1aa94b35b6f8b1
Kelton Hale <77764287+khale913@users.noreply.github.com>
2023-03-11 16:40:27 -0800

    search working

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -9,7 +9,33 @@ const Search = ({ searchTerm, setSearchTerm }) => {
   const [pins, setPins] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  return <div>Search</div>;
+  useEffect(() => {
+    if (searchTerm) {
+      console.log("search");
+      setLoading(true);
+      const query = searchQuery(searchTerm.toLowerCase());
+
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [searchTerm]);
+
+  return (
+    <div className="">
+      {loading && <Spinner message="Searching for pins..." />}
+      {pins?.length !== 0 && <MasonryLayout pins={pins} />}
+      {pins?.length === 0 && searchTerm !== "" && !loading && (
+        <div className="mt-10 text-center text-xl">No pins found</div>
+      )}
+    </div>
+  );
 };
 
 export default Search;
